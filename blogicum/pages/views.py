cdf838645c2f476805694afe880f49fbdfba778b
Николай Болотов <mrcharley216@gmail.com>
2025-02-01 20:35:45 +0500

    first time

---
+++ blogicum/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+app_name = 'pages'
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def rules(request):
+    return render(request, 'pages/rules.html')
